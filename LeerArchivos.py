d52a8ecf468e0213ccbe2795c1c8f3c6d7ce8b77
U-sepSick <usepsick@gmail.com>
2018-05-24 20:29:09 +0200

    Prueba Mod

--- LeerArchivos.py
+++ LeerArchivos.py
@@ -1,8 +1,20 @@
+import pygame
+from pygame.locals import *
+
+pygame.init()
+fpsClock = pygame.time.Clock()
+
+width = 1280
+height = 800
+
+surface = pygame.display.set_mode((width, height))
+
 contenido = open ('Ficheros.txt').readlines()
 
 contador = 0
 
 while contador < len(contenido):
+
 	elemento = contenido[contador].split()
 
 	if elemento[0] == "nave":
@@ -13,4 +25,9 @@ while contador < len(contenido):
 
 
 	contador = contador + 1
+
+while True:
+
+    pygame.display.update()
+    fpsClock.tick(30)
 
