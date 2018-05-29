c70d5d68c99c8b0db3e25ea8c35d53b385553bac
U-sepSick <usepsick@gmail.com>
2018-05-29 10:40:12 +0200

    Añadiendo imgenes

--- LeerArchivos.py
+++ LeerArchivos.py
@@ -14,20 +14,23 @@ contenido = open ('Ficheros.txt').readlines()
 contador = 0
 
 while contador < len(contenido):
+    elemento = contenido[contador].split()
 
-	elemento = contenido[contador].split()
+    if elemento[0] == "nave":
+        print ("Cargando grafico " + str(elemento[0]) + " => ", elemento[1])
+        nave_img = pygame.image.load(elemento[1])
+        nave_w, nave_h = nave_img.get_rect().size[0],nave_img.get_rect().size[1]
 
-	if elemento[0] == "nave":
-		print ("Cargando grafico " + str(elemento[0]) + " => ", elemento[1])
+    elif elemento[0] == "marcianito1":
+        print ("Cargando grafico " + str(elemento[0]) + " => " , elemento[1])
+        mar1_img = pygame.image.load(elemento[1])
+        mar1_w, mar1_h = mar1_img.get_rect().size[0],mar1_img.get_rect().size[1]
 
-	elif elemento[0] == "marcianito1":
-		print ("Cargando grafico " + str(elemento[0]) + " => " , elemento[1])
-
-
-	contador = contador + 1
+    contador = contador + 1
 
 while True:
+    surface.blit(mar1_img, (0,height/2-mar1_h/2))
+    surface.blit(nave_img, (width-nave_w, height/2-nave_h/2))
 
     pygame.display.update()
     fpsClock.tick(30)
-
